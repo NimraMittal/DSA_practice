dee881a08014bbf30ec54c14f5f96fffc2ba818a
Nimra Mittal <nimra.mittla.s.125@kalvium.community>
2026-05-07 15:16:35 +0530

    generateparantheses

---
+++ BackTracking/Genrate parantheses.py
@@ -0,0 +1,14 @@
+def generateParantheses(n):
+    res = []
+    def backtrack(path, open, close):
+        if len(path) == n*2:
+            res.append(path[:])
+            return
+        if open < n:
+            backtrack(path + "(", open+1, close)
+        if close < open:
+            backtrack (path + ")", open, close+1)
+    backtrack("", 0, 0)
+    return res
+
+print(generateParantheses(4))
