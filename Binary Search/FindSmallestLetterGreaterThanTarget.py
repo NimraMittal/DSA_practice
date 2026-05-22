2c44918981525e76bf80ed5baab40331748ddbef
Nimra Mittal <nimra.mittla.s.125@kalvium.community>
2026-05-22 13:34:55 +0530

    FindSmallestLetterGreaterThanTarget

---
+++ Binary Search/FindSmallestLetterGreaterThanTarget.py
@@ -0,0 +1,14 @@
+def nextGreatestLetter(letters,target):
+    low = 0 
+    high = len(letters)-1
+
+    ans = letters[0]
+    while low <= high:
+        mid = (low + high)//2
+
+        if letters[mid] > target:
+            ans = letters[mid]
+            high = mid - 1
+        else: 
+            low = mid + 1
+    return ans
