44e67ee3393bf74cd1ea882e0551c7dc5fa15c3a
Nimra Mittal <nimra.mittla.s.125@kalvium.community>
2026-05-23 20:35:11 +0530

    CountNegativeNumbersinaSortedMatrix

---
+++ Binary Search/CountNegativeNumbersinaSortedMatrix.py
@@ -0,0 +1,14 @@
+def countNegatives(grid):
+    m, n = len(grid), len(grid[0])
+    row = 0
+    col = n - 1
+    count = 0
+        
+    while row < m and col >= 0:
+        if grid[row][col] < 0:
+            count += (m - row)
+            col -= 1
+        else:
+            row += 1
+                
+    return count
